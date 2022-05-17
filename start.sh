fc76721606bd43753b880f3b9ae8252339ceff2a
PEACE_LOVE_TO_RIDE <80451254+shiva20991@users.noreply.github.com>
2022-05-17 21:55:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-if [[ -n $RCLONE_CONFIG ]]; then
- echo "Rclone config detected"
- echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+ wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
 fi
 
-bash setup.sh; python3 -m tobrot
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+ wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
+ unzip accounts.zip -d /usr/src/app/accounts
+ rm accounts.zip
+fi
+
+gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & python3 alive.py & ./aria.sh; python3 -m bot
